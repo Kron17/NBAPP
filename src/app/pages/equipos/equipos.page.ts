fe912300bbea75a1dac7f1cf4049c5604c3e8198
Kevin Bustos <ke.bustosr@duocuc.cl>
2023-09-15 10:56:20 -0300

    Se agrego un sweet alert.

--- src/app/pages/equipos/equipos.page.ts
+++ src/app/pages/equipos/equipos.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-equipos',
@@ -24,7 +25,7 @@ export class EquiposPage implements OnInit {
     this.httpClient.get<any>(url).subscribe(resultado => {
       //console.log(resultado);
       //console.log(resultado.content);
-      //this.digimones = this.digimones.concat(resultado.content);  Con el concat se ira cargando datos para abajo
+      //this.digimones = this.digimones.concat(resultado.content);  //Con el concat se ira cargando datos para abajo
       this.digimones = resultado.content;
       console.log(this.digimones)
     });
@@ -35,6 +36,17 @@ export class EquiposPage implements OnInit {
       console.log(this.personajes)
     });
     */
+   this.mensajeToast()
+  }
+
+  cargarMasData() {
+    this.paginaActual++;
+    const url = `https://www.digi-api.com/api/v1/digimon?page=${this.paginaActual}`
+    this.httpClient.get<any>(url).subscribe(resultado => {
+      this.digimones = this.digimones.concat(resultado.content);  //Con el concat se ira cargando datos para abajo
+      console.log(this.digimones)
+    });
+    this.mensajeToast()
   }
 
   cargarSiguientePagina() {
@@ -47,4 +59,32 @@ export class EquiposPage implements OnInit {
     this.cargarData();
   }
 
+  mensajeToast() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'bottom-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Carga exitosa!'
+    })
+  }
+
+  mensaje(){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+      heightAuto: false,
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+  }
 }
